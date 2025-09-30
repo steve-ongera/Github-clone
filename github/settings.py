89850635ac3ce38cde5d067c1717ca0ac3e9da4b
steve <gadafisteve001@gmail.com>
2025-09-30 12:07:25 +0300

    updates

--- github/settings.py
+++ github/settings.py
@@ -51,7 +51,7 @@ ROOT_URLCONF = 'github.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
